0ab5a363293d5e83cfaedd8cb835cb844efb1ee7
Tae-Eon <pte1236@gamil.com>
2022-03-25 09:32:49 +0000

    bug_fix

--- trainer/groupdro.py
+++ trainer/groupdro.py
@@ -28,7 +28,7 @@ class Trainer(trainer.GenericTrainer):
             
             self._train_epoch(epoch, train_loader, model,criterion)
 
-                eval_start_time = time.time()                
+            eval_start_time = time.time()                
             eval_loss, eval_acc, eval_deom, eval_deoa, _, _  = self.evaluate(self.model, 
                                                                              test_loader, 
                                                                              self.criterion,
@@ -38,12 +38,12 @@ class Trainer(trainer.GenericTrainer):
                                                                              writer=writer
                                                                             )
             eval_end_time = time.time()
-                print('[{}/{}] Method: {} '
-                      'Test Loss: {:.3f} Test Acc: {:.2f} Test DEOM {:.2f} [{:.2f} s]'.format
-                      (epoch + 1, epochs, self.method,
-                       eval_loss, eval_acc, eval_deom, (eval_end_time - eval_start_time)))
+            print('[{}/{}] Method: {} '
+                  'Test Loss: {:.3f} Test Acc: {:.2f} Test DEOM {:.2f} [{:.2f} s]'.format
+                  (epoch + 1, epochs, self.method,
+                   eval_loss, eval_acc, eval_deom, (eval_end_time - eval_start_time)))
 
-                if self.record:
+            if self.record:
                 self.evaluate(self.model, train_loader, self.criterion, epoch, 
                               train=True, 
                               record=self.record,
